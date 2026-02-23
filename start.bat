706dc68b9e9d38074e6ef05a4572151d8bace1e3
Eld-abs-001 <eld.abs.001@gmail.com>
2026-02-23 17:05:41 +0600

    Версия оригинала 2.7.6, /

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d C:\Windows\System32\Quanta
-C:\Windows\System32\Quanta\venv\Scripts\python.exe manage.py runserver 127.0.0.1:8004 --noreload
+cd /d C:\Programs\Quanta
+C:\Programs\Quanta\venv\Scripts\python.exe manage.py runserver 127.0.0.1:8004 --noreload
